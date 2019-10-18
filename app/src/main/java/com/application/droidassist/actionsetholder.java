47ff8ce01a78d8ef47585365fdab18f4880104cb
Rishi Pandey <hrishipandey06@gmail.com>
2019-10-18 23:54:48 +0530

    Main activity as splash page and new activity for list view of all action sets(actionsetholder.java). mainactivity is to designed properly
    Signed-off-by: Rishi Pandey <hrishipandey06@gmail.com>

--- app/src/main/java/com/application/droidassist/actionsetholder.java
+++ app/src/main/java/com/application/droidassist/actionsetholder.java
@@ -3,12 +3,16 @@ package com.application.droidassist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class actionsetholder extends AppCompatActivity {
+    ListView actionsetlistview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actionsetholder);
+
+        actionsetlistview = findViewById(R.id.actionsetlistview);
     }
 }
